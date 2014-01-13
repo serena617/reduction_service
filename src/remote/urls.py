713be2e1cab87058395f203e834cdbd13254dd95
Doucet, Mathieu <doucetm@ornl.gov>
2014-01-13 09:42:50 -0500

    Clean up urls

--- src/remote/urls.py
+++ src/remote/urls.py
@@ -4,7 +4,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^jobs/$', 'remote.views.query_remote_jobs'),
-    url(r'^authenticate/$', 'remote.views.authenticate'),
+    url(r'^authenticate/$', 'remote.views.authenticate', name='remote_authenticate'),
     url(r'^query/(?P<job_id>\d+)/$', 'remote.views.job_details', name='remote_job_details'),
     url(r'^download/(?P<trans_id>\d+)/(?P<filename>[\w\.]+)$', 'remote.views.download_file'),
 )
