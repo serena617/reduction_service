ab85d4d8f4c8be8e22c30e32c8760dfc0817199b
Mathieu Doucet <doucetm@ornl.gov>
2014-02-12 15:52:38 -0500

    Make sure there are no duplicates in the tool area

--- src/reduction_service/view_util.py
+++ src/reduction_service/view_util.py
@@ -8,6 +8,17 @@ def fill_template_values(request, **template_args):
     """
     template_args = users.view_util.fill_template_values(request, **template_args)
     template_args = remote.view_util.fill_template_values(request, **template_args)
-    template_args['reduction_apps'] = [{'name':'eqsans',
-                                        'url': reverse('eqsans.views.reduction_home')}]
+    
+    # It may be the case that we are currently viewing a part of the site
+    # belonging to an instrument-specific app. In this case, we'll already have
+    # an instrument entry in the dictionary. We should exclude that instrument.
+    instrument = None
+    if 'instrument' in template_args:
+        instrument = template_args['instrument']
+    reduction_apps = []
+    for instr in ['eqsans']:
+        if not instrument==instr:
+            reduction_apps.append({'name':instr,
+                                   'url': reverse('%s.views.reduction_home' % instr)})
+    template_args['reduction_apps'] = reduction_apps
     return template_args
