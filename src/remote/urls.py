82efa0067ef913fc8f7eb0c4b04bf591fc0640c7
Doucet, Mathieu <doucetm@ornl.gov>
2014-02-26 13:54:03 -0500

    Add easier way to delete remote transactions

--- src/remote/urls.py
+++ src/remote/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^jobs/$', 'remote.views.query_remote_jobs'),
+    url(r'^jobs/$', 'remote.views.query_remote_jobs', name='remote_query_jobs'),
     url(r'^authenticate/$', 'remote.views.authenticate', name='remote_authenticate'),
     url(r'^query/(?P<job_id>[\w\-\.]+)/$', 'remote.views.job_details', name='remote_job_details'),
     url(r'^download/(?P<trans_id>\d+)/(?P<filename>[\w\-\.]+)$', 'remote.views.download_file', name='remote_download'),
